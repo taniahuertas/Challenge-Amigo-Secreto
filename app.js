c538d52abe2189f5e75cd5024bddbf673856d788
Tania Huertas <taniaborraez@gmail.com>
2025-08-22 18:49:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+/* Declaración de variable */
+
+let Amigos = [];
+
+function agregarAmigo(nombre)
+{
+    nombre== "" ? console.log("No se puede agregar un amigo sin nombre") : Amigos.push(nombre);
+;} 
